bdeef70fb6041eaf65a96f622c814edc52ebfcd9
zhaanghao <ainimutiaraaa@gmail.com>
2025-05-14 07:57:34 +0700

    Delete landing page directory

--- landing page/searchform.php
+++
@@ -1,4 +0,0 @@
-<form method="get" id="searchform" class="searchform" action="<?php echo esc_url(home_url('/')); ?>">
-	<input placeholder="<?php esc_attr_e('Type Here...', 'fast-food-delivery'); ?>" type="text" name="s" id="search" value="<?php the_search_query(); ?>" />
-	<input type="submit" class="search-submit" value="<?php esc_attr_e('Search', 'fast-food-delivery');?>" />
-</form>
